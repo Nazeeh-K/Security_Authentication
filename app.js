ff949b973ba528dd8897d27db72b1956d3025e28
Nazeeh <117707020+Nazeeh-K@users.noreply.github.com>
2024-11-23 07:26:17 +0200

    remove MD5, add bcrypt

--- app.js
+++ app.js
@@ -3,7 +3,8 @@ const express = require('express');
 const bodyParser = require('body-parser');
 const ejs = require('ejs');
 const mongoose = require('mongoose');
-const md5 = require('md5');
+const bcrypt = require('bcrypt');
+const saltRounds = 10;
 
 const app = express();
 const port = 3000;
@@ -43,9 +44,11 @@ app.get("/register", (req, res) => {
 });
 
 app.post("/register", (req, res) => {
-    const newUser = new User ({
+
+    bcrypt.hash(req.body.password, saltRounds, (err, hash) => {
+        const newUser = new User ({
         email: req.body.username,
-        password: md5(req.body.password)
+        password: hash
     });
     
     newUser.save()
@@ -54,19 +57,22 @@ app.post("/register", (req, res) => {
         }) .catch((err) => {
             console.log(err);
         })
+    })
 });
 
 app.post("/login", (req, res) => {
     const username = req.body.username;
-    const password = md5(req.body.password);
+    const password = req.body.password;
 
     User.findOne({email: username})
         .then((foundUser) => {
             if (foundUser) {
-                if (foundUser.password === password) {
-                    res.render("secrets");
-                }
-            }
+                bcrypt.compare(password, foundUser.password, (err, result) => {
+                    if (result === true) {
+                        res.render("secrets");
+                    }
+                });
+            };
         }).catch((err) => {
             console.log(err);
         })
